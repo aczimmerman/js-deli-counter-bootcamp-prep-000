ce22d179e372192a5b75986b70ca9467614f1d75
Andrea Zimmerman <andrea.c.zimmerman@gmail.com>
2017-09-22 20:54:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ function takeANumber (katzDeliLine, person) {
   katzDeliLine.push(person)
   return `Welcome, ${person}. You are number ${katzDeliLine.length} in line.`
 }
+
+function nowServing(line) {
+  if (line.length === 0) {
+    return "There is nobody waiting to be served!"
+  }
+  return `Currently serving ${line.shift()}.`
+} 
