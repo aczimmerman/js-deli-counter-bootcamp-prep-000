a56d66d28c518ae0ed0e2043e449c537a56f61ec
Andrea Zimmerman <andrea.c.zimmerman@gmail.com>
2017-09-22 20:48:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function takeANumber (katzDeliLine, person) {
-  var katzDeliLine = [person];
   katzDeliLine.push(person)
   return `Welcome, ${person}. You are number ${katzDeliLine.length} in line.`
 }
